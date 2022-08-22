c48a292a5c7e1081a3bc23780be62ec5af89e2c3
Uriel SAUTRON <uriel.sautron@gmail.com>
2022-08-22 11:54:38 +0200

    Quest express 02 ok

---
+++ userHandlers.js
@@ -0,0 +1,34 @@
+const database = require("./database");
+
+const getUsers = (req, res) => {
+  database
+    .query("select * from users")
+    .then(([users]) => res.status(200).json(users))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("select * from users where id = ?", [id])
+    .then(([user]) => {
+      if (user[0]) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).send("User not found");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+};
