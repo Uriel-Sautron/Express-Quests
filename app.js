dbf1d07ea6edf29d0930646e0caa850a2d30bd75
Uriel SAUTRON <uriel.sautron@gmail.com>
2022-08-23 09:35:29 +0200

    Quest node 04 ok

--- app.js
+++ app.js
@@ -1,37 +1,5 @@
-const express = require("express");
 require("dotenv").config();
 
-const app = express();
-app.use(express.json());
-
-const port = process.env.APP_PORT ?? 5000;
-
-const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
-};
-
-app.get("/", welcome);
-
-const movieHandlers = require("./movieHandlers");
-
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.editMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
-
-// Routes Users
-const userHandlers = require("./userHandlers");
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", userHandlers.postUser);
-app.put("/api/users/:id", userHandlers.editUser);
-app.delete("/api/users/:id", userHandlers.deleteUser);
-
-app.listen(port, (err) => {
-  if (err) {
-    console.error("Something bad happened");
-  } else {
-    console.log(`Server is listening on ${port}`);
-  }
-});
+console.log(
+  `I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY}, and I love ${process.env.MY_LANGUAGE}`
+);
